286086293423f115239302a9c52eab235f824a15
Khamhuang, David <khameye19888@gmail.com>
2019-05-22 19:23:28 -0500

    timer is working!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,22 @@
+//setTimeout(fiveSeconds, 1000 * 5);
+//console.log (setTimeout)
 
+
+// Setting up the countdown from sixty to zero
+var timer = 60;
+
+var interval = setInterval(function() {
+    document.getElementById('watch').innerHTML = --timer;
+
+    if (timer <= 0)
+    {
+        document.getElementById('watch').innerHTML = 'TIME is UP!!!!!!';
+        clearInterval(interval);
+    }
+}, 1000);
+
+
+
+//function countDown () {
+  //  document.getElementById("watch").innerHTML = timer;
+//}
