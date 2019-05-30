715940403c52e81348bee926c28462ffb228478b
Khamhuang, David <khameye19888@gmail.com>
2019-05-29 22:27:35 -0500

    functioning quiz w/o timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,53 @@
 // Setting up the countdown from sixty to zero
-var timer = 60; 
+//var timer = 60; 
+//var interval = setInterval(function() {
+  //  document.getElementById('watch').innerHTML = --timer;
+//if (timer <= 0)
+  //  {
+    //    document.getElementById('watch').innerHTML = 'TIME is UP!!!!!!';
+      //  clearInterval(interval);
+  //  }
+//}, 1000);
 
-var interval = setInterval(function() {
-    document.getElementById('watch').innerHTML = --timer;
 
-    if (timer <= 0)
-    {
-        document.getElementById('watch').innerHTML = 'TIME is UP!!!!!!';
-        clearInterval(interval);
-    }
-}, 1000);
+
 
 function submitAnswers () {
     var total = 5;
     var score = 0;
 
     // Get User Input
-    var q1 =
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+
+    // Validation
+    for (i =1; i <= total; i++){
+        if (eval ("q"+ i) == null || eval("q" + i) == "") {
+            alert ("You missed question " + i);
+            return false;
+        }
+    }
+    
+//Setting Correct Answers
+
+var answers = ["d", "c", "d", "c", "a"];
+
+//Check Answers
+for (i =1; i <= total; i++){
+    if (eval ("q" + i) == answers[i - 1]){
+        score++;
+    }
+}
+
+//Display Results
+var results = document.getElementById("results");
+    results.innerHTML = "<h1> You scored <span>"+ score + "</span> out of " + total + "</span></h1>";
+alert("You scored  " + score + " out of  " + total);
+
+return false;
 }
 
 
